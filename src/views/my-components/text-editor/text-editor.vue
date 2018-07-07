b623f781b0302770eff78c4d7e9e5bc32e4a56e3
hll <920295151@qq.com>
2018-07-07 20:29:09 +0800

    tree-grid

--- src/views/my-components/text-editor/text-editor.vue
+++ src/views/my-components/text-editor/text-editor.vue
@@ -3,72 +3,126 @@
 </style>
 
 <template>
-    <div>
-        <Card shadow>
-            <textarea class='tinymce-textarea' id="tinymceEditer"></textarea>
-        </Card>
-        <Spin fix v-if="spinShow">
-            <Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>
-            <div>加载组件中...</div>
-        </Spin>
-    </div>
+    
+        <tree-grid 
+        :items='data' 
+        :columns='columns'
+        @on-row-click='rowClick'
+        @on-selection-change='selectionClick'
+        @on-sort-change='sortClick'
+      ></tree-grid>
+    
 </template>
 
 <script>
-import tinymce from 'tinymce';
+import TreeGrid from './treeGrid'
 export default {
     name: 'text-editor',
-    data () {
-        return {
-            spinShow: true
-        };
-    },
+     data() {
+            return {
+                columns: [{
+                    title: '权限名',
+                    key: 'name',
+                    width: '150',
+                }, {
+                    title: '权限',
+                    key: 'authority',
+                    width: '150',
+                },  {
+                    title: '是否激活',
+                    key: 'status',
+                    width: '150',
+                },  {
+                    title: '条件',
+                    key: 'condition',
+                    width: '150',
+                }, {
+                    title: '操作',
+                    type: 'action',
+                    actions: [{
+                        type: 'primary',
+                        text: '添加子权限'
+                    }, {
+                        type: 'success',
+                        text: '修改'
+                    },{
+                        type: 'error',
+                        text: '删除'
+                    }],
+                    width: '150',
+                }],
+                data: [{
+                    id: '1',
+                    name: '后台首页',
+                    authority: 'Admin/Index/index',
+                    status: '激活',
+                    condition: '',
+                    children: [{
+                        id: '01',
+                        name: '欢迎界面',
+                        authority: 'Admin/Index/welcome',
+                        status: '激活',
+                        condition: ''
+                    }]
+                }, {
+                    id: '2',
+                    name: '系统设置',
+                    authority: 'Admin/ShowNav/config',
+                    status: '激活',
+                    condition: '',
+                    children: [{
+                        id: '01',
+                        name: '菜单管理',
+                        authority: 'Admin/ShowNav/nav',
+                        status: '激活',
+                        condition: '',
+                        children: [{
+                        id: '001',
+                        name: '菜单列表',
+                        authority: 'Admin/Nav/index',
+                        status: '激活',
+                        condition: ''
+                    }]
+                    }]
+                }, {
+                    id: '3',
+                    name: '权限控制',
+                    authority: 'Admin/ShowNav/role',
+                    status: '激活',
+                    condition: '',
+                    children: [{
+                        id: '01',
+                        name: '权限管理',
+                        authority: 'Admin/Role/index',
+                        status: '激活',
+                        condition: ''
+                    }, {
+                    id: '02',
+                    name: '用户组管理',
+                    authority: 'Admin/Role/grroup',
+                    status: '激活',
+                    condition: ''
+                }]
+                }]
+            }
+        },
+         components: {
+            TreeGrid
+        },
     methods: {
-        init () {
-            this.$nextTick(() => {
-                let vm = this;
-                let height = document.body.offsetHeight - 300;
-                tinymce.init({
-                    selector: '#tinymceEditer',
-                    branding: false,
-                    elementpath: false,
-                    height: height,
-                    language: 'zh_CN.GB2312',
-                    menubar: 'edit insert view format table tools',
-                    plugins: [
-                        'advlist autolink lists link image charmap print preview hr anchor pagebreak imagetools',
-                        'searchreplace visualblocks visualchars code fullpage',
-                        'insertdatetime media nonbreaking save table contextmenu directionality',
-                        'emoticons paste textcolor colorpicker textpattern imagetools codesample'
-                    ],
-                    toolbar1: ' newnote print preview | undo redo | insert | styleselect | forecolor backcolor bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image emoticons media codesample',
-                    autosave_interval: '20s',
-                    image_advtab: true,
-                    table_default_styles: {
-                        width: '100%',
-                        borderCollapse: 'collapse'
-                    },
-                    setup: function (editor) {
-                        editor.on('init', function (e) {
-                            vm.spinShow = false;
-                            if (localStorage.editorContent) {
-                                tinymce.get('tinymceEditer').setContent(localStorage.editorContent);
-                            }
-                        });
-                        editor.on('keydown', function (e) {
-                            localStorage.editorContent = tinymce.get('tinymceEditer').getContent();
-                        });
-                    }
-                });
-            });
+            rowClick(data, index, event) {
+                console.log('当前行数据:' + data)
+                console.log('点击行号:' + index)
+                console.log('点击事件:' + event)
+            },
+            selectionClick(arr) {
+                console.log('选中数据id数组:' + arr)
+            },
+            sortClick(key, type) {
+                console.log('排序字段:' + key)
+                console.log('排序规则:' + type)
+            }
         }
-    },
-    mounted () {
-        this.init();
-    },
-    destroyed () {
-        tinymce.get('tinymceEditer').destroy();
-    }
 };
 </script>
 
