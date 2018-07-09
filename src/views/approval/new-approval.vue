8de539715347a0e59fe53f2a34839560bd252363
yeyongkai501 <yeyongkai501@163.com>
2018-07-10 06:29:46 +0800

    design of approval almalmost ok

--- src/views/approval/new-approval.vue
+++ src/views/approval/new-approval.vue
@@ -1,55 +1,90 @@
-<style lang="less">    
+<style lang="less">
     @import 'draggable-list.less';
 </style>
 <template>
     <div>
         <Row>
-            表单设置<br>
-            审批名称最多50字
-            <Input v-model="approvalName" placeholder="Enter something..." style="width: 300px"></Input>
-
-            审批说明最多100字
-            <Input v-model="approvalComment" placeholder="Enter something..." style="width: 300px"></Input>
-
-            <br>
-            <UserGroup cardtitle="授权分组" v-on:update-selected="getSelectedGroupIds"></UserGroup>
-            
-            <AllUsers  cardtitle="授权用户" v-on:update-selected="getSelectedUserIds"></AllUsers>
-                        
+            <Col span="20" style="text-align:center;font-size:24px;color: #2db7f5;">
+                <span>审批设计器</span>
+            </Col>
+            <Col span="2">
+            <Button type="primary" @click="save_and_use">保存并启用</Button>
+            </Col>
         </Row>
         <Row>
-            <Modal width="700" v-model="add_new_field" title="添加字段">
-                <ApprovalFieldsForm v-bind:initIndex="choosedDataIndex" v-bind:initData="choosedData" v-on:form-data-hash="getFieldData"></ApprovalFieldsForm>
-                <div slot="footer"></div>
-            </Modal>
             <Card>
-                <div slot="title">
-                    <Icon type="compose"></Icon>
-                    字段列表
-                    <Button @click="add_new_field = true" type="primary">添加字段</Button>
-                </div>               
+                <p slot="title">
+                    <Icon type="android-funnel"></Icon>
+                    表单设置
+                </p>
+                <div>
+                    审批名称最多50字
+                    <Input v-model="approvalName" placeholder="Enter something..." style="width: 300px"></Input>
 
-                <Table :columns="approval_field_col" :data="approval_field_data"></Table>
-            
+                    审批说明最多100字
+                    <Input v-model="approvalComment" placeholder="Enter something..." style="width: 300px"></Input>
+                </div>
             </Card>
         </Row>
         <Row>
+            <UserGroup cardtitle="授权分组" v-on:update-selected="getSelectedGroupIds"></UserGroup>
+        </Row>
+        <Row>    
+            <AllUsers  cardtitle="授权用户" v-on:update-selected="getSelectedUserIds"></AllUsers>
+                        
+        </Row>
+
+        <Row>
             <Col span="8" class="padding-left-10">
                 <Card>
-                    <p slot="title">
+                    <p slot="title" style="height: 33px;">
                         <Icon type="android-funnel"></Icon>
-                        字段列表预览
+                        字段列表
+                        <Button type="primary" @click="addNewField">添加新字段</Button>
+
                     </p>
                     <div style="height: 394px;">
             
                         <ul id="editable" class="iview-admin-draggable-list">
                                 
                             <li v-for="(item, index) in approval_field_data" :key="index" class="notwrap todolist-item" :data-index="index">
-                            {{ item.name }}<Icon type="close" class="js-remove"/><Icon type="edit" class="js-edit" /></li>
+                            {{ item.name }}<Icon type="close" class="js-remove"/></li>
                         </ul>
-
-                        <!-- <button id="addUser">Add</button> -->
-                        <Button @click="add_new_field = true" type="primary">添加字段</Button>
+                        
+                    </div>
+                </Card>
+            </Col>
+            <Col span="10" class="padding-left-10">
+                <Card>
+                    <p slot="title" style="height: 33px;">
+                        <Icon type="android-funnel"></Icon>
+                        字段设置
+                    </p>
+                    <div style="height: 394px;">
+                        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+                            <FormItem label="Name" prop="name">
+                                
+                                <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+                                    
+                            </FormItem>
+                            <FormItem label="En_name" prop="en_name">
+                                <Input v-model="formValidate.en_name" placeholder="Enter your en_name"></Input>
+                            </FormItem>
+                            <FormItem label="Control" prop="control">
+                                <Select v-model="formValidate.control" placeholder="Select your control">
+                                    <Option value="单行输入框">单行输入框</Option>
+                                    <Option value="多行输入框">多行输入框</Option>
+                                    <Option value="单选框">单选框</Option>
+                                    <Option value="数字输入框">数字输入框</Option>
+                                    <Option value="多选框">多选框</Option>
+                                    <Option value="日期">日期</Option>
+                                    <Option value="图片">图片</Option>
+                                    <Option value="金额">金额</Option>
+                                    
+                                </Select>
+                            </FormItem>
+                          
+                        </Form>
                     </div>
                 </Card>
             </Col>
@@ -62,15 +97,15 @@
 <script>
 import UserGroup from "../my-components/user-and-group/user-group.vue"
 import AllUsers from "../my-components/user-and-group/all-users.vue"
-import ApprovalFieldsForm from "./approval-fields-form.vue"
 import Sortable from 'sortablejs';
 export default {
     name: 'new-approval-tmpl',
     data () {
         return {
+            editFlag: false,
+            editIndex: -1,
             add_new_field: false,
-            choosedDataIndex: '',
-            choosedData: '',
+
             approval_field_col: [
                 // {
                 //     title: '编号',
@@ -127,33 +162,49 @@ export default {
             
             selected_user_group_ids: [],            
             selected_user_ids: [],
+            formValidate: {
+                name: '',
+                field_position: '',
+                en_name: '',
+                control: '',                
+            },
+            newFromValidate: {
+                name: '名称',
+                field_position: '111',
+                en_name: '字段英文名称',
+                control: '单行输入框',                
+            },
+            ruleValidate: {
+                name: [
+                    { required: true, message: 'The name cannot be empty', trigger: 'blur' }
+                ],
+                field_position: [
+                    { required: true, message: 'Please select', trigger: 'blur' }
+                    
+                ],
+                en_name: [
+                    { required: true, message: 'The name cannot be empty', trigger: 'change' }
+                ],
+                control: [
+                    { required: true, message: 'Please select', trigger: 'change' }
+                ],
+               
+            }
             
         };
     },
     components:{
-        UserGroup,AllUsers,ApprovalFieldsForm
+        UserGroup,AllUsers
     },
     mounted () {
         let editable = document.getElementById('editable');
         let vm = this;
         var editableList = Sortable.create(editable, {
-            filter: '.js-remove, .js-edit',
+            filter: '.js-remove',
             onFilter: function (evt) {
-                
-                if(evt.target.attributes.class.textContent.indexOf('js-remove')>=0){
-                    var el = editableList.closest(evt.item); // get dragged item
-                    // el && el.parentNode.removeChild(el);  //delete the <li>...</li>
-                    // debugger
-                    vm.approval_field_data.splice(parseInt(el.attributes["data-index"]),1)
-                }
-                if(evt.target.attributes.class.textContent.indexOf('js-edit')>=0){
-                    var el = editableList.closest(evt.item);
-                    vm.choosedDataIndex = el.attributes["data-index"].value;
-                    // debugger
-                    vm.choosedData = JSON.stringify(vm.approval_field_data[parseInt(el.attributes["data-index"].value)]);
-                    vm.add_new_field = true; //open the modal dialog
-                }
-
+                var el = editableList.closest(evt.item); // get dragged item
+                // el && el.parentNode.removeChild(el);
+                vm.approval_field_data.splice(parseInt(el.getAttribute('data-index')),1)
             },
             onChoose: function (/**Event*/evt) {
                 // el and evt.item is equal
@@ -171,24 +222,11 @@ export default {
                     }                    
                 }
                 
+                vm.editIndex = parseInt(el.attributes["data-index"].value)
+                vm.editFlag = true
+                vm.formValidate = vm.approval_field_data[vm.editIndex]        
             },
-            // Changed sorting within list
-            // onUpdate: function (/**Event*/evt) {
-            //     // same properties as onEnd
-            //     var el = editableList.closest(evt.item);
-            //     console.log(el)
-            //     // console.log(JSON.stringify(vm.approval_field_data[0]));
-            //     // console.log(JSON.stringify(vm.approval_field_data[1]));
-            //     var t;
-            //     t = vm.approval_field_data[evt.oldIndex];
-            //     vm.approval_field_data[evt.oldIndex] = vm.approval_field_data[evt.newIndex];
-            //     vm.approval_field_data[evt.newIndex] = t;
-            //     // console.log(JSON.stringify(vm.approval_field_data[0]));
-            //     // console.log(JSON.stringify(vm.approval_field_data[1]));
-            //     // evt.oldIndex;  // element's old index within old parent
-            //     // evt.newIndex;  // element's new index within new parent
-
-            // },
+            
         });
 
     },
@@ -199,17 +237,31 @@ export default {
         getSelectedUserIds(msg){
             this.selected_user_ids = msg;
         },
-        getFieldData(msg){
-            if(this.choosedDataIndex){
-                this.approval_field_data[parseInt(this.choosedDataIndex)] =msg;
-            }else{
-                this.approval_field_data.push(msg);
-            }
-            
-        }
-        // add_new_field(){
 
-        // },
+        handleSubmit (name) {
+            this.$refs[name].validate((valid) => {
+                if (valid) { 
+                    //JSON.parse(JSON.stringify  for deep copy                   
+                    this.approval_field_data.push(JSON.parse(JSON.stringify(this.formValidate)));
+                    
+                    // debugger
+                    this.$Message.success('保存成功！');
+                } else {
+                    this.$Message.error('数据格式不正确！');
+                   
+                }
+            })
+        },
+        handleReset (name) {
+            this.$refs[name].resetFields();
+        },
+        addNewField(){
+            this.approval_field_data.push(JSON.parse(JSON.stringify(this.newFromValidate)));
+        },
+        save_and_use(){
+            console.log(this.approval_field_data)
+        }
+
         
     }
 };
