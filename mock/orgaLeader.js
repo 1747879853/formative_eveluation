af017ac47f3aed692ff2238461838948babbeb76
SavageLiu <3502285053@qq.com>
2018-07-24 17:23:19 +0800

    save()

--- mock/orgaLeader.js
+++ mock/orgaLeader.js
@@ -1,131 +1,134 @@
+
 import Mock from 'mockjs'
 
+let arr;
+let data1;
+var id = 0;
+var maxid = 10;
+let params;
+var newid=5;
 
-let data1 = [           
-                {
-                    id:1,
-                    title: '权限组1',
-                    checked: false,                         
-                },
-                {
-                    id:2,
-                    title: '权限组2',
-                    checked: false,
-                },
-                {
-                    id:3,
-                    title: '权限组3',
-                    checked: false,
-                },
-                {
-                    id:4,
-                    title: '权限组4',
-                    checked: false,
-                }
-            ]
-
-let arr = [
+  let list = function (options) {
+   /*模拟删除数据的方式*/
+   let rtype = options.type.toLowerCase(); //获取请求类型
+   switch (rtype) {
+     case 'get':
+        arr = [
           {
               id: '1',
               name: '董事长',
-              condition: '111',
-              status: '激活',
-              leaf: 0,
-              checked_id:[1,3],
+              checked_id:[4],
               children: [
                   {
                       id: '2',
                       name: '总经理',
-                      condition: '222',
-                      status: '激活',
+                      checked_id:[3],
                       leaf: 1,
-                      checked_id:[1,3],
                       children:[{
                       id: '3',
                       name: '副总经理',
-                      condition: '222',
-                      status: '激活',
+                      checked_id:[2],
                       leaf: 2,
-                      checked_id:[1,3],
                       children: []
                   },
                   {
                       id: '4',
                       name: '副总经理',
-                      condition: '222',
-                      status: '激活',
-                      leaf: 2,
-                      checked_id:[1,3],
+                      checked_id:[1],
                       children: [
                           {
                           id: '5',
                           name: '生产部经理',
-                          condition: '222',
-                          status: '激活',
+                          checked_id:[4],
                           leaf: 2,
-                          checked_id:[1,3],
                           children: []
                           },
                           {
                           id: '6',
-                          authority: 'Admin/Index/welcome',
                           name: '销售部经理',
-                          condition: '222',
-                          status: '激活',
-                          leaf: 2,
-                          checked_id:[1,3],
+                          checked_id:[3],
                           children: []
                           }
                       ]
                   },
                   ]
                   },
-                  // {
-                  //     id: '6',
-                  //     authority: 'Admin/Index/welcome',
-                  //     name: '欢迎页面',
-                  //     condition: '222',
-                  //     status: '激活',
-                  //     leaf: 1,
-                  //     children: []
-                  // }
               ]
           },
       ]
-
-let list = function (options) {
-   /*模拟删除数据的方式*/
-   let rtype = options.type.toLowerCase(); //获取请求类型
-   switch (rtype) {
-     case 'get':    
+        data1 = [
+                {
+                    id:1,
+                    title: 'leader1',
+                    checked: false,                         
+                },
+                {
+                    id:2,
+                    title: 'leader2',
+                    checked: false,
+                },
+                {
+                    id:3,
+                    title: 'leader3',
+                    checked: false,
+                },
+                {
+                    id:4,
+                    title: 'leader4',
+                    checked: false,
+                }
+              ]
+        return {a: arr, b: data1}
        break;
-     case 'post'://添加新用户
-       var newid=5;     
-       var newarr = 
-        {
-            id : newid,
-            name : JSON.parse(options.body).params.name, 
-            checked_id : []             
-        };
-        arr.push(newarr);
-        newid=newid+1;
-       break;
-     case 'delete':
-            arr.splice(parseInt(JSON.parse(options.body).params.id),1);
-        break;
-     case 'patch': //修改data1中的checked_id
-        let i=parseInt(JSON.parse(options.body).params.user_id);
-        var newarr;
-        arr[i].checked_id=JSON.parse(options.body).params.id;
-        newarr=arr[i];
-        break;
+     case 'post'://添加新组织机构
+            id = maxid;
+            maxid = maxid + 1;
+            params = JSON.parse(options.body).params;
+            arr = {
+                id: id,
+                name: params.name,
+                checked_id: [],
+                children: []
+            };
+            return arr;
+     case 'patch'://修改权限
+        // params = JSON.parse(options.body).params;
+        //     arr = {
+        //         id: params.id,
+        //         name: params.name,
+        //     };
+        // return arr;
+         params = JSON.parse(options.body).params;
+            arr = {
+                id: params.id,
+                name: params.name,
+                checked_id:params.checked_id,
+            };
+            data1={
+              id:params.checked_id;
+              checked:params.checked;
+            }
+        return [arr,data1];
+        
+        // var newarr;
+        // arr[i].checked_id=JSON.parse(options.body).params.checked_id;
+        // newarr=arr[i];
+        // break;
+     case 'delete'://删除权限
+        params = JSON.parse(options.body).params;
+            arr = {
+                id: params.id
+            };
+        return arr; 
      default:
    }
 
    return [data1,arr,newarr];//返回这个数组,也就是返回处理后的假数据
 
  }
+
+
 export default {
-    path: '/authUserList',
-    data: list,
+    path: '/organization',
+    data: list
 }
