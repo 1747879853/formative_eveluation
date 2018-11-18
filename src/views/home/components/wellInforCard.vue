2dcb6e9e0aaafbb9cdfaccb985b22ef73040a10c
Liu ZS <lzfuzi@foxmail.com>
2018-11-18 14:58:55 +0800

    home page html

---
+++ src/views/home/components/wellInforCard.vue
@@ -0,0 +1,52 @@
+<style lang="less">
+    @import './styles/infor-card.less';
+</style>
+
+<template>
+    <Card :padding="0">
+        <Row class="height-100" type="flex" align="middle" justify="center">
+            <Icon type="ios-eye" size="50"></Icon>
+        </Row>
+        <Row  align="middle" justify="center" class="height-100">
+            <p class="infor-intro-text">{{ wellName }}</p>
+            <p class="infor-intro-text">{{ startStatus }}</p>
+            <p class="infor-intro-text">{{ sensor }}</p>
+            <p class="infor-intro-text">{{ flulidProduction }}</p>
+            <p class="infor-intro-text">{{ startStatus }}</p> 
+        </Row>
+    </Card>
+</template>
+
+<script>
+export default {
+    name: 'inforCard',
+    props: {
+        well_id: String,
+        wellName: String,
+        //开停机状态 -1 0 1
+        isStart: Number,
+        // sensor
+        sensor: Array,
+        flulidProduction: Number,
+    },
+    computed: {
+        color(){
+            if (this.isStart == 1) {
+                return "yellow";
+            }else if(this.isStart == -1){
+                return "red";
+            }
+            return "green";
+        },
+        startStatus(){
+            if (this.isStart == 1) {
+                return "当日停机";
+            }else if(this.isStart == -1){
+                return "长停";
+            }
+            return "正在运行";  
+        }
+    },
+};
+</script>
+
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,42 +5,42 @@
   <div>
     <Card :style="height">
       <div>
+        <div style="height: 5px;"></div>
         请选择开停机状态：
-        <CheckboxGroup v-model="sensor_type_Group">
-            <Checkbox style="width: 100px;" label="sensor_type">
+        <CheckboxGroup v-model="isStart_Group">
+            <Checkbox style="width: 100px;" label="starting">
               <span>开机状态</span>
             </Checkbox>
-            <Checkbox style="width: 100px;" label="sensor_type">
+            <Checkbox style="width: 100px;" label="stoped">
               <span>停机状态</span>
             </Checkbox>
-            <Checkbox style="width: 100px;" label="sensor_type">
+            <Checkbox style="width: 100px;" label="stoping">
               <span>当日停机状态</span>
             </Checkbox>
         </CheckboxGroup>
       </div>
       <div>
+        <div style="height: 5px;"></div>
         请选择区块：
         <cascaderMulti style="width: 300px" v-model="end_code" :data="end_codes" placeholder="区块"></cascaderMulti>
       </div>
       <div :style="style">
         <div>
+          <div style="height: 5px;"></div>
           请选择传感器类型：
           <CheckboxGroup v-model="sensor_type_Group">
               <Checkbox style="width: 100px;flex-warp: wrap;" v-for="sensor_type in sensor_type_data" :label="sensor_type" :key="sensor_type">
-                <span>{{sensor_type}}</span>
               </Checkbox>
           </CheckboxGroup>
         </div>
-        </span><span v-for="sensor_type in sensor_type_Group">{{sensor_type}}</span>
         <div>
+          <div style="height: 5px;"></div>
           请选择油井类型：
           <CheckboxGroup v-model="well_type_Group">
               <Checkbox style="width: 100px;flex-warp: wrap;" v-for="well_type in well_type_data" :label="well_type" :key="well_type">
-                <span>{{well_type}}</span>
               </Checkbox>
           </CheckboxGroup>
         </div>
-        </span><span v-for="sensor_type in sensor_type_Group">{{sensor_type}}</span>
       </div> 
       <br>
       <div class="example-code-more" @click="click">
@@ -48,75 +48,121 @@
           <Icon type="ios-arrow-up" v-show="showMore"></Icon>
       </div>
     </Card>
-    <Button type="primary" style="float:right;">重置</Button>
-    <Button type="primary" style="float:right;">确定</Button>
+    <div style="height: 5px;"></div>
+    <div style="height: 35px;">
+      <Button style="float:left;">
+        <Icon type="ios-arrow-down" v-show="!showIsStart"></Icon>
+        <Icon type="ios-arrow-up" v-show="showIsStart"></Icon>
+        <span>开停状态</span>
+      </Button>
+      <Button style="float:left;">
+        <Icon type="ios-arrow-down" v-show="!showWellType"></Icon>
+        <Icon type="ios-arrow-up" v-show="showWellType"></Icon>
+        <span>油井类型</span>
+      </Button>
+      <Button style="float:left;">
+        <Icon type="ios-arrow-down" v-show="!showFP"></Icon>
+        <Icon type="ios-arrow-up" v-show="showFP"></Icon>
+        <span>产液量</span>
+      </Button>
+      
+      <Button type="primary" style="float:right;">重置</Button>
+      <Button type="primary" style="float:right;">确定</Button>
+    </div>
+    <Row :gutter="5">
+        <Col :xs="24" :sm="12" :md="6" :style="{marginBottom: '10px'}" v-for="item in wellinfor" :key=item.well_id>
+            <infor-card
+                :wellName=item.well_name
+                :isStart=item.isStart
+                :sensor=item.sensor
+                :flulidProduction=item.flulidProduction
+            ></infor-card>
+        </Col>
+      </Row>
   </div>
 </template>
 <script>
+import inforCard from "./components/wellInforCard.vue";
     export default {
-        data () {
-            return {
-              isExpand: true,
-              showMore: true,
-              // e_height: 120,
-              u_height: 150,
-              end_code: [],
-              end_codes: [
-                {
-                  value: 1000,
+      name: "home",
+      components: {
+        inforCard,
+      },
+      data () {
+          return {
+            isExpand: true,
+            showMore: true,
+            u_height: 150,
+            showIsStart: false,
+            showWellType: false,
+            showFP: false,
+            isStart_Group: [],
+            end_code: [],
+            end_codes: [
+              {
+                value: 1000,
+                label: "区块",
+                multiple: true,
+                children: [{
                   label: "区块",
-                  multiple: true,
-                  children: [{
-                    label: "区块",
-                    value: 1100,
-                    children: [],
-                    multiple: true //可忽略项，当为true时该项为多选
-                  },{
-                    label: "区块",
-                    value: 1100,
-                    children: [],
-                    multiple: true //可忽略项，当为true时该项为多选
-                  },
-                  {
-                    label: "区块",
-                    value: 1100,
-                    children: [],
-                    multiple: true //可忽略项，当为true时该项为多选
-                  }]
-                }
-              ],
-              sensor_type_Group: [],
-              sensor_type_data: ["s1","s12","s13","s15","s16","s17","s18","s117","2s18","4s17","5s18","fs17","ds18","sf17","s1s8","sh17","sv18","s17f","s18h","s17s","s1e8"],
-              well_type_Group: [],
-              well_type_data: ["s1","s12","s13","s15","s16","s17","s18","as517","s1845","45s17","s4518","s1745","s18ry","s15s7","s5418","s1j67","sj6518","s1756","jtes18","s1e6j7","ejs18"],
-              checkAllGroup:[],
-            }
-        },
-        methods: {
-          click(){
-            this.showMore = !this.showMore;
-            this.isExpand = !this.isExpand;
+                  value: 1100,
+                  children: [],
+                  multiple: true //可忽略项，当为true时该项为多选
+                },{
+                  label: "区块",
+                  value: 1100,
+                  children: [],
+                  multiple: true //可忽略项，当为true时该项为多选
+                },
+                {
+                  label: "区块",
+                  value: 1100,
+                  children: [],
+                  multiple: true //可忽略项，当为true时该项为多选
+                }]
+              }
+            ],
+            sensor_type_Group: [],
+            sensor_type_data: ["s1","s12","s13","s15","s16","s17","s18","s117","2s18","4s17","5s18","fs17","ds18","sf17","s1s8","sh17","sv18","s17f","s18h","s17s","s1e8"],
+            well_type_Group: [],
+            well_type_data: ["s1","s12","s13","s15","s16","s17","s18","as517","s1845","45s17","s4518","s1745","s18ry","s15s7","s5418","s1j67","sj6518","s1756","jtes18","s1e6j7","ejs18"],
+            checkAllGroup:[],
+            // {well_id:"",well_naem:"",isStart:"",sensor:"",flulidProduction:},
+            wellinfor:[
+              {well_id:"well-1",well_name:"well-1",isStart:1,sensor:['传感器1','传感器1','传感器1','传感器1'],flulidProduction:150},
+              {well_id:"well-12",well_name:"well-12",isStart:0,sensor:['传感器1','传感器1','传感器1','传感器1'],flulidProduction:200},
+              {well_id:"well-13",well_name:"well-13",isStart:1,sensor:['传感器1','传感器1','传感器1','传感器1'],flulidProduction:300},
+              {well_id:"well-14",well_name:"well-14",isStart:-1,sensor:['传感器1','传感器1','传感器1','传感器1'],flulidProduction:300},
+              {well_id:"well-15",well_name:"well-15",isStart:-1,sensor:['传感器1','传感器1','传感器1','传感器1'],flulidProduction:300},
+              {well_id:"well-16",well_name:"well-16",isStart:-1,sensor:['传感器1','传感器1','传感器1','传感器1'],flulidProduction:300},
+            ],
           }
-        },
-        computed: {
-            height () {
-                let style = {};
-                if (this.isExpand) {
-                    // style.height = `${this.e_height}px`;
-                } else {
-                    style.height = `${this.u_height}px`;
-                }
-                return style;
-            },
-            style () {
-                let style = {
-                    opacity: 1
-                };
-                if (!this.isExpand) {
-                    style.opacity = 0;
-                }
-                return style;
-            },
-        },
+      },
+      methods: {
+        click(){
+          this.showMore = !this.showMore;
+          this.isExpand = !this.isExpand;
+        }
+      },
+      computed: {
+          height () {
+              let style = {};
+              if (this.isExpand) {
+                  // style.height = `${this.e_height}px`;
+              } else {
+                  style.height = `${this.u_height}px`;
+              }
+              return style;
+          },
+          style () {
+              let style = {
+                  opacity: 1
+              };
+              if (!this.isExpand) {
+                  style.opacity = 0;
+              }
+              return style;
+          },
+      },
     }
 </script>
