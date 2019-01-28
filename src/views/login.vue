9e614a4560ccb52d2dcd7650c7a525c2a433628e
“owl <576359186@qq.com>
2019-01-28 22:04:18 +0800

    login经历了注释和回复

--- src/views/login.vue
+++ src/views/login.vue
@@ -56,8 +56,8 @@ export default {
                     { required: true, message: '密码不能为空', trigger: 'blur' }
                 ]
             },
-            failed:'',
-            pad:false
+            failed: '',
+            pad: false
         };
     },
     methods: {
@@ -75,58 +75,58 @@ export default {
                     // }
                     // this.$router.push({
                     //     name: 'home_index'
-                    // });  
-                                     
-                    this.$axios.post("/user_token", {
-                        auth:{
+                    // });
+
+                    this.$axios.post('/user_token', {
+                        auth: {
                             email: this.form.userName,
                             password: this.form.password
-                        }  
-                    }).then(function(res) {
-                        this.$store.commit('set_token', res.data.jwt); 
+                        }
+                    }).then(function (res) {
+                        this.$store.commit('set_token', res.data.jwt);
                         console.log(res.data);
-                        this.$store.commit('set_auth_rules', res.data.auth_rules);  
+                        this.$store.commit('set_auth_rules', res.data.auth_rules);
                         if (this.$store.state.token) {
                             Cookies.set('user', this.form.userName);
                             Cookies.set('userid', 6);
                             Cookies.set('password', this.form.password);
                             this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
                             Cookies.set('access', 0);
-                            if(this.pad&&res.data.banzu){
-                                this.$router.replace('/padteam');  
-                            }else{
-                            this.$router.push({ 
-                                name: 'home_index'
-                            });
-                        }
+                            if (this.pad && res.data.banzu) {
+                                this.$router.replace('/padteam');
+                            } else {
+                                this.$router.push({
+                                    name: 'home_index'
+                                });
+                            }
                         } else {
-                            // this.$router.replace('/login');  
-                        }                     
+                            // this.$router.replace('/login');
+                        }
                     }.bind(this))
-                    .catch(error => {
-                        console.log(error);
-                        this.failed='登录失败,请输入正确的用户名和密码！';      
-                    })
+                        .catch(error => {
+                            console.log(error);
+                            this.failed = '登录失败,请输入正确的用户名和密码！';
+                        });
 
 
 
                 }
             });
         },
-    _isMobile() {
-      let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
-      return flag;
-    }
+        _isMobile () {
+            let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+            return flag;
+        }
     },
-    mounted (){
-        this.form.userName=Cookies.get('user');
-        if(this._isMobile()){
-         this.pad = true;
-        }else{
-         this.pad = false;
+    mounted () {
+        this.form.userName = Cookies.get('user');
+        if (this._isMobile()) {
+            this.pad = true;
+        } else {
+            this.pad = false;
         }
 
-        
+
     }
 };
 </script>
