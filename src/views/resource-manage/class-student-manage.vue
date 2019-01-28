e7cf9b1f12c9e52fa4181ad2846254d7f997706d
owl <576359186@qq.com>
2019-01-28 21:56:42 +0800

    class student admin 2ramin

--- src/views/resource-manage/class-student-manage.vue
+++ src/views/resource-manage/class-student-manage.vue
@@ -5,24 +5,25 @@
             班级列表&nbsp;&nbsp;&nbsp;
             <!--<Button @click="show_modal1()" class="ivu-btn ivu-btn-primary ivu-btn-small">添加班级</Button>-->
         <div>
+
             <Modal
                     v-model="modal1"
+                    title="添加学生信息"
+                    @on-ok="addclassstu"
+                    @on-cancel="cancel2">
+                <table>
+                    <tr><td>学号</td><td>
+                        <Input v-model="stuid" placeholder="请输入学生学号" clearable style="width: 300px"></Input></td></tr>
+                </table>
+            </Modal>
+            <Modal
+                    v-model="modal2"
                     width="65%"
                     :mask-closable="false"
                     title="删除该班学生"
                     @on-ok="ok"
                     @on-cancel="cancel">
                 <Table :columns="classstuColumns" :data="classstuData" style="width: 100%;"></Table>
-            </Modal>
-            <Modal
-                    v-model="modal2"
-                    title="添加学生信息"
-                    @on-ok="ok2"
-                    @on-cancel="cancel2">
-                <table>
-                    <tr><td>学号</td><td>
-                        <Input v-model="name" placeholder="请输入学生学号" clearable style="width: 300px"></Input></td></tr>
-                </table>
             </Modal>
         </div>
         </p>
@@ -32,47 +33,45 @@
 
 <script>
     export default {
-        name: "user",
-        data() {
+        name: 'user',
+        data () {
             return {
-                modal1:false,
-                modal2:false,
+                modal1: false,
+                modal2: false,
                 id: 0,
-                name:'',
-                status:'',
-                startyear:'',
+                name: '',
+                status: '',
+                startyear: '',
                 userColumns: [
                     {
-                        type: "index",
-                        title: "序号",
+                        type: 'index',
+                        title: '序号',
                         width: 60
                     },
                     {
-                        title: "班级号",
-                        key: "id",
-                        align: "center"
+                        title: '班级号',
+                        key: 'id',
+                        align: 'center'
                     },
                     {
-                        title: "班级名",
-                        key: "name",
-                        align: "center"
-                    },
-
-
-                    {
-                        title: "状态",
-                        key: "status",
-                        align: "center",
+                        title: '班级名',
+                        key: 'name',
+                        align: 'center'
                     },
                     {
-                        title: "入学年份",
-                        key: "startyear",
-                        align: "center"
+                        title: '状态',
+                        key: 'status',
+                        align: 'center'
                     },
                     {
-                        title: "操作",
-                        key: "action",
-                        align: "center",
+                        title: '入学年份',
+                        key: 'startyear',
+                        align: 'center'
+                    },
+                    {
+                        title: '操作',
+                        key: 'action',
+                        align: 'center',
                         render: (h, params) => {
                             return h('div', [
                                 h('Button', {
@@ -85,7 +84,7 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.show_modal2(params.index);
+                                            this.addstu(params.index);
                                         }
                                     }
                                 }, '添加学生'),
@@ -96,7 +95,7 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.show_modal1()(params.index);
+                                            this.delstu()(params.index);
                                         }
                                     }
                                 }, '删除学生')
@@ -108,29 +107,29 @@
                 userData: [],
                 classstuColumns: [
                     {
-                        type: "index",
-                        title: "序号",
+                        type: 'index',
+                        title: '序号',
                         width: 60
                     },
                     {
-                        title: "姓名",
-                        key: "name",
-                        align: "center"
+                        title: '姓名',
+                        key: 'name',
+                        align: 'center'
                     },
                     {
-                        title: "学号",
-                        key: "id",
-                        align: "center"
+                        title: '学号',
+                        key: 'id',
+                        align: 'center'
                     },
                     {
-                        title: "入学年份",
-                        key: "startyear",
-                        align: "center"
+                        title: '入学年份',
+                        key: 'startyear',
+                        align: 'center'
                     },
                     {
-                        title: "操作",
-                        key: "action",
-                        align: "center",
+                        title: '操作',
+                        key: 'action',
+                        align: 'center',
                         render: (h, params) => {
                             return h('div', [
                                 h('Button', {
@@ -140,7 +139,7 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.show_modal1()(params.index);
+                                            this.delclassstu()(params.index);
                                         }
                                     }
                                 }, '删除')
@@ -149,17 +148,17 @@
                     }
 
                 ],
-                classstuData: [],
+                classstuData: []
             };
         },
         computed: {
-            avatorImage() {
+            avatorImage () {
                 return localStorage.avatorImgPath;
             }
         },
-        mounted() {
+        mounted () {
             this.$axios
-                .get("/classList")
+                .get('/classList')
                 .then(res => {
                     this.userData = res.data;
                 })
@@ -167,14 +166,25 @@
                     console.log(error);
                 });
         },
-        methods:{
-            show_modal1()
-            {
-                this.modal1=true;
-                this.name="";
-                this.status="";
+        methods: {
+            addstu (index) { // 显示增加学生的窗口
+                this.modal1 = true;
+                this.id = this.userData[index].id;
+                this.name = this.userData[index].name; // 这应该是获取了班级名称
+                alert(this.name);
+            },
+            delstu (index) { // 显示删除学生的窗口
+                this.modal2 = true;
+                this.name = '';
+                this.status = '';
+                // this.id = this.userData[index].id;
+                alert(this.id);// 全局获取到班级id，失败了，加上上面的一句就加载不出来
                 this.$axios
-                    .get("/classStu")
+                    .get('/classStu', {// 根据班级id获取学生列表
+                        params: {
+                            id: index
+                        }
+                    })
                     .then(res => {
                         this.classstuData = res.data;
                     })
@@ -182,96 +192,67 @@
                         console.log(error);
                     });
             },
-            ok ()
-            {
-                this.$axios.post('/classList', {
+            addclassstu () {
+                this.$axios.post('/AddClassStu', { // 添加学生窗口的确定按钮
                     params: {
-                        name: this.name,
-                        status: 1,
+                        id: this.id, // 班级的id
+                        stuid: this.stuid// 学号，怎么获取这个input的输入？现在是不是已经获取了
                     }
-                }).then(function(res) {
-                    console.log(res.data);
-                    this.userData.push(res.data);
-                    this.$Message.info('添加成功');
-                }.bind(this))
-                    .catch(function(error) {
-                        console.log(error)
-                    });
-
-            },
-            show_modal2(index)
-            {
-                this.modal2=true;
-                this.id = this.userData[index].id;
-                this.name=this.userData[index].name;
-            },
-            ok2 ()
-            {
-                this.$axios.patch('/classList', {
-                    params: {
-                        id: this.id,
-                        name: this.name,
-                    }
-                }).then(function(res) {
-                    console.log(res);
-                    let id = res.data.id;
-                    for(let i = 0; i < this.userData.length; i++){
-                        if (this.userData[i].id == id){
-                            this.userData[i].name = res.data.name;
-                            break;
-                        }
-                    }
+                }).then(function (res) {
+                    // console.log(res);
+                    // let id = res.data.id;
+                    // for (let i = 0; i < this.userData.length; i++) {
+                    //     if (this.userData[i].id == id) {
+                    //         this.userData[i].name = res.data.name;
+                    //         break;
+                    //     }
+                    // }
                     this.$Message.info('修改成功');
                 }.bind(this))
-                    .catch(function(error) {
-                        console.log(error)
+                    .catch(function (error) {
+                        console.log(error);
                     });
-
             },
-            cancel2 ()
-            {this.$Message.info('取消');},
-            deleteClick(index)
-            {
-                this.id = this.userData[index].id;
+            delclassstu (index) { // 删除学生里面的按钮
                 this.$Modal.confirm({
-                    title: '删除班级',
-                    content: '<p>确定要删除此班级吗？</p>',
+                    title: '移除学生',
+                    content: '<p>从班级中移除此学生吗?</p>',
                     onOk: () => {
-                        this.$axios.delete('/classList', {
-                            data: {
-                                params: {
-                                    id: this.id,
-                                    status: 0
-                                }
+                        this.$axios.post('/DelClassStu', {
+                            params: {
+                                stuid: this.classstuData[index].id, // 获取这一行的学号
+                                classid: this.id // 全局的班级id，刚才没获取成功
                             }
-                        }).then(function(res) {
-                            console.log(res);
-                            this.userData.splice(index,1);
-                            this.$Message.info('删除成功');
+                        }).then(function (res) {
+                            console.log(res.data);
+                            this.userData.push(res.data);
+                            this.$Message.info('移除成功');
                         }.bind(this))
-                            .catch(function(error) {
-                                console.log(error)
+                            .catch(function (error) {
+                                console.log(error);
                             });
-
                     },
                     onCancel: () => { this.$Message.info('取消'); }});
             },
 
-            userBody(row, column, index) {
-                return row[column.key]
+            ok () { this.$Message.info('成功'); },
+            cancel2 () { this.$Message.info('取消'); },
+
+            userBody (row, column, index) {
+                return row[column.key];
             },
-            userName(row, index) {
-                return row["name"]
+            userName (row, index) {
+                return row['name'];
             },
-            user_Name(row, index) {
-                return row["user_name"]
+            user_Name (row, index) {
+                return row['user_name'];
             },
-            userTel(row, index) {
-                return row["user_tel"]
+            userTel (row, index) {
+                return row['user_tel'];
             },
-            userStatus(row, index) {
-                return row["status"]
-            },
+            userStatus (row, index) {
+                return row['status'];
+            }
 
         }
     };
