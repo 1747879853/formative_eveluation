34eecc41b1239691f8931ff6b6b48579bc95b26d
yyd <1747879853@qq.com>
2019-09-30 12:27:45 +0800

    home.vue添加了详情按钮，点击详情按钮弹出详情对话框。details_histogram.vue是详情组件

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -9,6 +9,7 @@
                 <p slot="title">
                 上周即时评价统计
                 </p>
+                <Button @click="show_details_histo()">详情</Button>
                 <histo-gram></histo-gram>
             </Card>
         </Col>
@@ -18,27 +19,38 @@
                 <h1>欢迎使用本系统</h1>
             </Col>
         </Row>
-        
+    <Modal v-model="show_details" width="1500px" height="1000px" :closable="false">
+        <details-histogram ></details-histogram>
+    </Modal>
         
         
     </div>
 </template>
 <script>
 import histoGram from './components/histogram.vue';
+import detailsHistogram from './components/details_histogram.vue'
 export default {
     name: 'home',
     components: {
-        histoGram //上周提问活跃度——柱状图
+        histoGram, //上周提问活跃度——柱状图
+        detailsHistogram //活跃度详情
     },
     data () {
         return { 
-            show_histo:false
-           
+            show_histo:false,
+            show_details:false
+        }
+    },
+    methods:{
+        show_details_histo() {
+            this.show_details = true
         }
     },
     mounted() {
         this.$axios.get("/show_histo").then( res =>{
-            this.show_histo = res.data.show            
+            this.show_histo = res.data
+            console.log("sdfsadfsdf")
+            console.log(res.data)          
         }).catch(error =>{
             console.log(error);
         });
